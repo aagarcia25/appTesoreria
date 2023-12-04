48584f207c3b35881f9e3211dc22bc0db8e64b21
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-12-04 11:44:07 -0600

    version 1

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import {Platform} from 'react-native';
+import {PERMISSIONS, RESULTS, check, request} from 'react-native-permissions';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  useEffect(() => {
+    const requestDownloadPermission = async () => {
+      try {
+        const permissions =
+          Platform.OS === 'android'
+            ? PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE
+            : PERMISSIONS.IOS.PHOTO_LIBRARY;
+
+        const permissionStatus = await request(permissions);
+        if (permissionStatus === RESULTS.GRANTED) {
+          console.log('Permiso de descarga concedido');
+        } else {
+          console.log('Permiso de descarga denegado');
+        }
+      } catch (error) {
+        console.error('Error al solicitar permiso de descarga:', error);
+      }
+    };
+
+    const checkDownloadPermission = async () => {
+      try {
+        const permissions =
+          Platform.OS === 'android'
+            ? PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE
+            : PERMISSIONS.IOS.PHOTO_LIBRARY;
+
+        const permissionStatus = await check(permissions);
+        if (permissionStatus !== RESULTS.GRANTED) {
+          requestDownloadPermission();
+        } else {
+          console.log('Permiso de descarga ya concedido');
+        }
+      } catch (error) {
+        console.error('Error al verificar permiso de descarga:', error);
+      }
+    };
+
+    checkDownloadPermission();
+  }, []);
+
+  return (
+    <WebView
+      style={{flex: 1}}
+      source={{uri: 'https://tesoreria-virtual.nl.gob.mx/'}}
+    />
+  );
+};
+
+export default App;
